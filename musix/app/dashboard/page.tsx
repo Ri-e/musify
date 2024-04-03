21d147a95c046311a7f8e9bfbc4d0bb1dcfc2ba7
Ri-e <woxxiss1611@gmail.com>
2024-04-04 02:03:11 +0530

    aaj mein bhot khaya

--- musix/app/dashboard/page.tsx
+++ musix/app/dashboard/page.tsx
@@ -1,11 +1,17 @@
-import Sidebar from "../components/Sidebar"
+import Sidebar from "../components/Sidebar";
+import Card from "../components/Card";
+import fetchSongnames from "../components/data";
+export default async function page() {
 
-const page = () => {
   return (
-    <div className="bg-black h-screen w-full text-red-600 text-[25px]">
+    
+    <div className="bg-black h-screen w-full text-red-600 text-[25px] flex gap-4">
       <Sidebar />
+      <div>
+        <div>
+          <Card />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-
-export default page
