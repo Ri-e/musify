4cc3e6a71877d1333f609feed2afda31611cf420
Ri-e <woxxiss1611@gmail.com>
2024-04-09 04:00:56 +0530

    aaj mein bhot lhaya

---
+++ musix/app/components/data.ts
@@ -0,0 +1,12 @@
+// import { sql } from "@vercel/postgres";
+
+// export  default async function fetchSongnames() {
+//   try {
+//     const data = await sql`SELECT name, artist, song_image_url FROM songs`;
+//     return data.rows;
+//   } catch (err) {
+//     console.error("error caught : ", err);
+//     throw new Error("failed to fetch songs names");
+//   }
+// }
+
---
+++ musix/next.config.js
@@ -0,0 +1,8 @@
+// next.config.js
+
+module.exports = {
+    images: {
+      domains: ['m.media-amazon.com', 'merchbar.imgix.net','c.files.bbci.co.uk'],
+    },
+  };
+  
