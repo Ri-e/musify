39ea71c892b0581e4faf303e9180fee0a6001a29
Ri-e <woxxiss1611@gmail.com>
2024-04-15 12:57:27 +0530

    aaj mein bhot lhaya

--- app/components/Video_player.tsx
+++ app/components/Video_player.tsx
@@ -1,10 +1,31 @@
-import fetchSongnames from "./data";
+'use client'
+import { IoPlayForward } from "react-icons/io5";
+import ReactAudioPlayer from 'react-audio-player';
 
-export default async function Video_player() {
-  const fetchData = fetchSongnames;
+import { FaPause } from "react-icons/fa";
+
+import { IoPlayBack } from "react-icons/io5";
+import { FaPlay } from "react-icons/fa";
+import { useState } from "react";
+export default function Video_player() {
+  
+  const [playing, setPlaying] = useState(true)
+  let play = () => {
+    setPlaying(!playing)
+  };
+  
   return (
-    <div className="fixed bottom-0 left-0 w-full bg-red-600 h-[5rem]">
-      <p>Hello</p>
+    <div className="fixed bottom-0 left-0 w-full bg-red-600 h-[5rem] text-white flex gap-[2.5rem] items-center justify-center ">
+      <IoPlayBack className="cursor-pointer hover:scale-[1.2] transition-all ease-in-out " />
+      {playing ? (
+        <FaPlay onClick={ play} className="cursor-pointer text-[30px] hover:scale-[1.2] transition-all ease-in-out " />
+      ) : (
+        <FaPause onClick={ play} className="cursor-pointer text-[30px] hover:scale-[1.2] transition-all ease-in-out " />
+        
+      )}
+
+      <IoPlayForward className="cursor-pointer hover:scale-[1.2] transition-all ease-in-out " />
+      <ReactAudioPlayer src="https://music.youtube.com/watch?v=aLSolb9bMBA&list=RDAMVMyjCAM-1R6w4" controls/>
     </div>
   );
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,11 @@
 import Sidebar from "../components/Sidebar";
 import Card from "../components/Card";
 import Video_player from "../components/Video_player";
-export default async function page() {
 
+export default async function page() {
+  
   return (
+    
     
     <div className="bg-red-100 h-[55rem] text-red-600 text-[25px] flex gap-4 scrollbar-hidden">
       <Sidebar />
@@ -11,6 +13,8 @@ export default async function page() {
         <div className="">
           <Card />
         </div>
+        
+
       </div>
       <Video_player/>
     </div>
